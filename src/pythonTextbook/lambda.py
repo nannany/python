36c81f0a6a802fd13ca6fffcbaee70bf2d0d8ba9
nannany <ymym1990ymym@gmail.com>
2017-02-09 18:19:04 +0900

    lambda

--- src/pythonTextbook/lambda.py
+++ src/pythonTextbook/lambda.py
@@ -1,5 +1,14 @@
 # coding:utf-8
 nums = [1, 3, 4, 4, 6]
+# map
 print(list(map(lambda a: a + 3, nums)))
 print(list(map(lambda a: a * 3, nums)))
 print(list(map(lambda a: a // 3, nums)))
+# filter
+print(list(filter(lambda x: (x % 2) == 0, nums)))
+print(list(filter(lambda x: (x % 2) == 1, nums)))
+# iter
+i = iter(nums)
+print(next(i))
+print(next(i))
+print(next(i))
