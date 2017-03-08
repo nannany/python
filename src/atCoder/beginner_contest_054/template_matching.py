45fd8fb5edbd37c02bc7e89a72ab93f9271af885
nannany <ymym1990ymym@gmail.com>
2017-03-09 00:31:38 +0900

    atcoder 途中まで

---
+++ src/atCoder/beginner_contest_054/template_matching.py
@@ -0,0 +1,11 @@
+first_line = input().split()
+n = first_line[0]
+m = first_line[1]
+
+n_list = []
+for i in range(1, n):
+    n_list.append(input())
+
+m_list = []
+for i in range(1, m):
+    m_list.append(input())
