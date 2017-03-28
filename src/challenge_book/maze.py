5f6449018a5a3d3a14308927e78ea63925358e26
nannany <ymym1990ymym@gmail.com>
2017-03-28 22:43:40 +0900

    修正。完成。

--- src/challenge_book/maze.py
+++ src/challenge_book/maze.py
@@ -22,28 +22,31 @@ dy = [0, 1, 0, -1]
 
 
 def bfs():
-    q = deque([[sx, sy]])
-    for i in range(1, N + 1):
+    count = 0
+    backup = []
+    for tmp_goal in range(1, N + 1):
         d = [[INF for i in range(W)] for j in range(H)]
-        if i == 1 :
+        if tmp_goal == 1:
+            q = deque([[sx, sy]])
             d[sx][sy] = 0
-        else :
+        else:
+            q = deque([backup])
+            d[backup[0]][backup[1]] = count
 
         while len(q) != 0:
-            tmp_q = q.popleft()
-            print(tmp_q)
-            tmp_x = tmp_q[0]
-            print("x" + str(tmp_x))
-            tmp_y = tmp_q[1]
-            print(tmp_y)
-            if maze[tmp_x][tmp_y] == str(i): break
+            tmp_x, tmp_y = q.popleft()
+            if maze[tmp_x][tmp_y] == str(tmp_goal):
+                count = d[tmp_x][tmp_y]
+                backup = [tmp_x, tmp_y]
+                q.clear()
+                break
             for j in range(0, 4):
                 nx = tmp_x + dx[j]
                 ny = tmp_y + dy[j]
                 if 0 <= nx < H and 0 <= ny < W and maze[nx][ny] != "X" and d[nx][ny] == INF:
                     q.append([nx, ny])
                     d[nx][ny] = d[tmp_x][tmp_y] + 1
-    return d[gx][gy]
+    return count
 
 
 if __name__ == '__main__':
