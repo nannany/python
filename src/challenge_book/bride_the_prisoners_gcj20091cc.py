664df9bb8fa2022c29d9fa6090882574fb453f6e
nannany <ymym1990ymym@gmail.com>
2017-05-01 22:31:44 +0900

    gcj

--- src/challenge_book/bride_the_prisoners_gcj20091cc.py
+++ src/challenge_book/bride_the_prisoners_gcj20091cc.py
@@ -1,8 +1,25 @@
+import sys
 import queue
 
 if __name__ == '__main__':
     T = int(input())
-    P, Q = list(map(int, input().split()))
-    A = list(map(int, input().split()))
+    for caseNo in range(1, T + 1):
+        P, Q = list(map(int, input().split()))
+        A = list(map(int, input().split()))
+        A.insert(0, 0)
+        A.append(P + 1)
 
-    que = queue.PriorityQueue()
+        dp = [[100000000 for i in range(0, Q + 2)] for j in range(Q + 1)]
+        for i in range(0, Q + 1):
+            dp[i][i + 1] = 0
+
+        for w in range(2, Q + 2):
+            for i in range(0, Q - w + 2):
+                j = i + w
+                t = 100000000
+                for k in range(i + 1, j):
+                    t = min(t, dp[i][k] + dp[k][j])
+
+                dp[i][j] = t + A[j] - A[i] - 2
+
+        print("Case #" + str(caseNo) + ": " + str(dp[0][Q + 1]))
