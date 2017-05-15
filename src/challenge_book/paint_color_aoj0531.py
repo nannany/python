d176c645a35ef207ec210061aea01b15e131ed28
nannany <ymym1990ymym@gmail.com>
2017-05-16 01:15:26 +0900

    途中まで。あと幅優先探索

--- src/challenge_book/paint_color_aoj0531.py
+++ src/challenge_book/paint_color_aoj0531.py
@@ -1,31 +1,47 @@
-def compress(_x1, _x2, _w):
+def compress(x1, x2, _w):
     xs = []
 
     for i in range(0, N):
         for d in range(-1, 2):
-            tx1 = _x1[i] + d
-            tx2 = _x2[i] + d
-            if 1 <= tx1 <= _w:
+            tx1 = x1[i] + d
+            tx2 = x2[i] + d
+            if 0 <= tx1 <= _w:
                 xs.append(tx1)
-            if 1 <= tx2 <= _w:
+            if 0 <= tx2 <= _w:
                 xs.append(tx2)
 
     xs = list(set(xs))
     xs.sort()
 
     for i in range(0, N):
+        x1[i] = xs.index(x1[i])
+        x2[i] = xs.index(x2[i])
+
+    return len(xs)
 
 
 if __name__ == '__main__':
     W, H = list(map(int, input().split()))
     N = int(input())
-    x1 = []
-    x2 = []
-    y1 = []
-    y2 = []
+    X1 = []
+    X2 = []
+    Y1 = []
+    Y2 = []
     for i in range(0, N):
         inputs = list(map(int, input().split()))
-        x1.append(inputs[0])
-        x2.append(inputs[1])
-        y1.append(inputs[2])
-        y2.append(inputs[3])
+        X1.append(inputs[0])
+        X2.append(inputs[1])
+        Y1.append(inputs[2])
+        Y2.append(inputs[3])
+
+    W = compress(X1, X2, W)
+    H = compress(Y1, Y2, H)
+
+    fld = [False for i in range(6 * N)]
+
+    for i in range(0, N):
+        for y in range(Y1[i], Y2[i]):
+            for x in range(X1[i], X2[i]):
+                fld[y][x] = True
+
+    ans = 0
