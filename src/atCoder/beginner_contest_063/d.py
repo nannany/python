6aec6bb7b9e303d4d28eea84d57022c7d44f7249
nannany <ymym1990ymym@gmail.com>
2017-06-04 11:50:19 +0900

    d。違うけどとりあえず

--- src/atCoder/beginner_contest_063/d.py
+++ src/atCoder/beginner_contest_063/d.py
@@ -1,7 +1,30 @@
+def C(_mid):
+    tmp = [x - B * _mid for x in h]
+    explode_count = 0
+    for i in range(0, len(tmp)):
+        if tmp[i] <= 0:
+            pass
+        else:
+            explode_count += int(tmp[i] / (A - B))
+
+    if _mid < explode_count:
+        return True
+    else:
+        return False
+
+
 if __name__ == '__main__':
     N, A, B = list(map(int, input().split()))
 
     h = [int(input()) for i in range(N)]
 
-    ans = 0
+    lb = 0
+    ub = 1000000000
+    while ub - lb > 1:
+        mid = int((lb + ub) / 2)
+        if C(mid):
+            lb = mid
+        else:
+            ub = mid
 
+    print(lb)
