6ae52ff5ebbc7f4db2d14f7b2cb3cdc87c9203c3
nannany <ymym1990ymym@gmail.com>
2017-06-25 22:03:34 +0900

    d途中

--- src/atCoder/beginner_contest_065/d.py
+++ src/atCoder/beginner_contest_065/d.py
@@ -1,18 +1,41 @@
+class Edge:
+    def __init__(self, u, v, cost):
+        self.u = u
+        self.v = v
+        self.cost = cost
+        # self.n = n
+
+
+class Coordinate:
+    def __init__(self, x, y, n):
+        self.x = x
+        self.y = y
+        self.n = n
+
+    def __repr__(self):
+        return repr((self.x, self.y))
+
+
 if __name__ == '__main__':
     N = int(input())
     x = []
     y = []
-    for i in range(N):
+    Coordinates = []
+    for i in range(0, N):
         tmp_x, tmp_y = list(map(int, input().split()))
-        x.append(tmp_x)
-        y.append(tmp_y)
+        # x.append(tmp_x)
+        # y.append(tmp_y)
+        Coordinates.append(Coordinate(tmp_x, tmp_y, i))
 
-    x_0 = x[0]
-    y_0 = y[0]
-    x = [ele - x_0 for ele in x]
-    y = [ele - y_0 for ele in y]
-    print(x)
-    print(y)
+    print(sorted(Coordinates, key=lambda coordinate: coordinate.y))
 
-    solve_list = [min(ele_x, ele_y) for ele_x in x for ele_y in y]
-    print(solve_list)
+    sorted_by_x = sorted(Coordinates, key=lambda coordinate: coordinate.x)
+    sorted_by_y = sorted(Coordinates, key=lambda coordinate: coordinate.y)
+
+    Edges = []
+    for i in range(0, N - 1):
+        cost_x = min(abs(sorted_by_x[i].x - sorted_by_x[i + 1].x), abs(sorted_by_x[i].y - sorted_by_x[i + 1].y))
+        Edges.append(Edge(sorted_by_x[i].n, sorted_by_x[i + 1].n, cost_x))
+        cost_y = min(abs(sorted_by_y[i].x - sorted_by_y[i + 1].x), abs(sorted_by_y[i].y - sorted_by_y[i + 1].y))
+        Edges.append(Edge(sorted_by_y[i].n, sorted_by_y[i + 1].n, cost_y))
+
