e68f5a67b3a38de37222d9cd8b231f6736b0dcdc
nannany <ymym1990ymym@gmail.com>
2017-07-24 22:31:06 +0900

    ac

--- src/atCoder/grand_contest_018/b.py
+++ src/atCoder/grand_contest_018/b.py
@@ -3,5 +3,18 @@ if __name__ == '__main__':
     A = []
     for i in range(N):
         A.append(list(map(int, input().split())))
-        
 
+    ans = float("inf")
+    while len(A[0]) != 0:
+        count_list = [0 for i in range(M + 1)]
+        for i in range(0, N):
+            count_list[A[i][0]] += 1
+
+        max_value = max(count_list)
+        for i in range(0, N):
+            A[i].remove(count_list.index(max_value))
+
+        if ans > max_value:
+            ans = max_value
+
+    print(ans)
