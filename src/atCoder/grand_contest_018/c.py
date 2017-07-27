f2787b0bdf7d6e54943d55be8d602955a534b5a6
nannany <ymym1990ymym@gmail.com>
2017-07-27 23:40:19 +0900

    途中。左と右で独立に計算すべきっぽい。

--- src/atCoder/grand_contest_018/c.py
+++ src/atCoder/grand_contest_018/c.py
@@ -1,3 +1,5 @@
+import heapq
+
 if __name__ == '__main__':
     X, Y, Z = list(map(int, input().split()))
     A = []
@@ -10,11 +12,21 @@ if __name__ == '__main__':
         B.append(tmp_b)
         C.append(tmp_c)
 
-    gold_minus_silver = [a - b for a in A for b in B]
-
+    gold_minus_silver = [(a - b, a, b, c) for (a, b, c) in zip(A, B, C)]
+    gold_minus_silver.sort()
+    print(gold_minus_silver)
 
     left_side = []
+    ans = 0
     for i in range(0, Y):
-        heappush(left_side, )
-    right_side = []
+        heapq.heappush(left_side, (
+            gold_minus_silver[i][2] - gold_minus_silver[i][3], gold_minus_silver[i][2], gold_minus_silver[i][3]))
+
+    left_max = [0 for i in range(Z)]
+
     for K in range(Y, Y + Z + 1):
+
+    right_side = []
+    for i in range(Y, N):
+        heapq.heappush(right_side, gold_minus_silver[i][1] - gold_minus_silver[i][3], gold_minus_silver[i][1],
+                       gold_minus_silver[i][3])
