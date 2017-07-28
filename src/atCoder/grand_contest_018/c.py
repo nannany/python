1fe446ea51c9591278a83fd05eefb3fbe0e1c8cc
nannany <ymym1990ymym@gmail.com>
2017-07-28 23:59:43 +0900

    tochu

--- src/atCoder/grand_contest_018/c.py
+++ src/atCoder/grand_contest_018/c.py
@@ -23,10 +23,20 @@ if __name__ == '__main__':
             gold_minus_silver[i][2] - gold_minus_silver[i][3], gold_minus_silver[i][2], gold_minus_silver[i][3]))
 
     left_max = [0 for i in range(Z)]
+    left_bronze = []
+    for K in range(0, Z):
+        heapq.heappush(left_side, gold_minus_silver[K + Y][2] - gold_minus_silver[K + Y][3],
+                       gold_minus_silver[K + Y][2],
+                       gold_minus_silver[K + Y][3])
+        left_bronze.append(heapq.heappop(left_side))
+        tmp_max = 0
+        for i in range(0, Y):
+            tmp_max += left_side[i][1]
 
-    for K in range(Y, Y + Z + 1):
+        for i in range(0,len(left_bronze)):
+            tmp_max += left_bronze[i][2]
 
-    right_side = []
-    for i in range(Y, N):
-        heapq.heappush(right_side, gold_minus_silver[i][1] - gold_minus_silver[i][3], gold_minus_silver[i][1],
-                       gold_minus_silver[i][3])
+        right_side = []
+        for i in range(Y, N):
+            heapq.heappush(right_side, gold_minus_silver[i][1] - gold_minus_silver[i][3], gold_minus_silver[i][1],
+                           gold_minus_silver[i][3])
