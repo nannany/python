08d4604ccbc0f340db93b8249d16a592eb677053
nannany <ymym1990ymym@gmail.com>
2017-08-12 00:29:03 +0900

    計算途中でmodとってやるとACに

--- src/yukicoder/554.py
+++ src/yukicoder/554.py
@@ -1,17 +1,18 @@
 if __name__ == '__main__':
     N = int(input())
-
+    MOD = 1e9 + 7
+    memo = [-1 for i in range(N + 1)]
+    memo[1] = 1
     sum_odd = 1
     sum_even = 0
     for i in range(2, N + 1):
         if i % 2 == 0:
-            sum_even += sum_odd * i
+            memo[i] = i * sum_odd % MOD
+            sum_even += memo[i]
+            sum_even %= MOD
         else:
-            sum_odd += sum_even * i
+            memo[i] = i * sum_even % MOD
+            sum_odd += memo[i]
+            sum_odd %= MOD
 
-    if N % 2 == 0:
-        print((sum_odd * N) % 1000000007)
-    elif N == 1:
-        print(1)
-    else:
-        print((sum_even * N) % 1000000007)
+    print(int(memo[N]))
