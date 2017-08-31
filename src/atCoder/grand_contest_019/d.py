fe0728b1da8901a34f291e85fbe3b241de1dc213
nannany <ymym1990ymym@gmail.com>
2017-08-31 23:19:02 +0900

    junbi

--- src/atCoder/grand_contest_019/d.py
+++ src/atCoder/grand_contest_019/d.py
@@ -1,3 +1,9 @@
 if __name__ == '__main__':
-    A = list(input().split())
-    B = list(input().split())
+    A = list(input())
+    B = list(input())
+    if B.count("1") == 0:
+        if A.count("1") == 0:
+            print(0)
+        else:
+            print(-1)
+
