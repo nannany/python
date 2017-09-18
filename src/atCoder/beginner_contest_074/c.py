dcbf88796f9d31269df9808580b5db6073829f32
nannany <ymym1990ymym@gmail.com>
2017-09-18 21:37:37 +0900

    junbi

--- src/atCoder/beginner_contest_074/c.py
+++ src/atCoder/beginner_contest_074/c.py
@@ -1,28 +1,31 @@
 if __name__ == '__main__':
     A, B, C, D, E, F = list(map(int, input().split()))
-    M = []
-    # A
-    for a_ in range(0, 31):
-        # B
-        for b_ in range(0, 31):
-            # C
-            for c_ in range(0, 31):
-                # D
-                for d_ in range(0, 31):
-                    if (c_ + d_) <= E * (a_ + b_) and 100 * (a_ + b_) + c_ + d_ <= F:
-                        if not (a_ == 0 and b_ == 0):
-                            M.append((a_, b_, c_, d_))
-                    else:
-                        break
-    print(M)
-    max_concent = 0
-    for a, b, c, d in M:
-        if max_concent < (c + d) / (a + b):
-            ans = (a, b, c, d)
-            max_concent = (c + d) / (a + b)
 
-    sum_g = 100 * (ans[0] + ans[1]) + ans[2] + ans[3]
-    print(ans)
-    sugar_g = ans[2] + ans[3]
+    # A+B のアリエル値
+    a_plus_b = set()
+    for i in range(0, 31, A):
+        for j in range(0, 31, B):
+            if not (i == 0 and j == 0):
+                if i + j <= 30:
+                    a_plus_b.add(i + j)
 
-    print(str(sum_g) + " " + str(sugar_g))
+    c_plus_d = set()
+    for i in range(0, F, C):
+        for j in range(0, F, D):
+            if i + j <= F:
+                c_plus_d.add(i + j)
+
+    max_concent = -1
+    ans_sum = 0
+    ans_sugar = 0
+    # print(c_plus_d)
+    # print(a_plus_b)
+    for cd in c_plus_d:
+        for ab in a_plus_b:
+            if cd <= E * ab and cd <= F - 100 * ab:
+                if max_concent <= cd / ab:
+                    max_concent = cd / ab
+                    ans_sum = 100 * ab + cd
+                    ans_sugar = cd
+
+    print(str(ans_sum) + " " + str(ans_sugar))
---
+++ src/atCoder/beginner_contest_074/d.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    N = int(input())
+    A = [list(map(int, input().split())) for i in range(N)]
+    # print(A)
