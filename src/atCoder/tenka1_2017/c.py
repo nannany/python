ed9eefd1f0c18f1d16221940779ffed646d39cae
nannany <ymym1990ymym@gmail.com>
2017-09-30 23:00:22 +0900

    ac

--- src/atCoder/tenka1_2017/c.py
+++ src/atCoder/tenka1_2017/c.py
@@ -1,3 +1,9 @@
 if __name__ == '__main__':
     N = int(input())
-    
+    for h in range(1, 3501):
+        for n in range(1, 3501):
+            if 4 * h * n - N * (n + h) > 0 and (N * h * n) % (4 * h * n - N * (n + h)) == 0:
+                w = (N * h * n) // (4 * h * n - N * (n + h))
+                print(str(h) + " " + str(n) + " " + str(w))
+                exit()
+
