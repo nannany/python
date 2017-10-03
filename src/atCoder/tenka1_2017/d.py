3ff5d05688151795b62a60c65b773169d9650431
nannany <ymym1990ymym@gmail.com>
2017-10-04 00:38:42 +0900

    ac

--- src/atCoder/tenka1_2017/d.py
+++ src/atCoder/tenka1_2017/d.py
@@ -6,7 +6,10 @@ if __name__ == '__main__':
     for i in range(N):
         integer, value = list(map(int, input().split()))
         integer_value.append((integer, value))
-    r = int(math.log(K, 2))
+    if K == 0:
+        r = 0
+    else:
+        r = int(math.log(K, 2))
     max_value = 0
     for i in range(r + 1):
         # print((K >> i) & 0b1)
@@ -14,13 +17,17 @@ if __name__ == '__main__':
             continue
 
         tmp_sum = 0
+
+        mask = 1
+        for j in range(i):
+            mask = (mask << 1) + 1
+        Ki = (K | mask) - 2 ** i
+
         for tmp_int, tmp_val in integer_value:
-            print(K - (1 << i))
-            if tmp_int < (1 << i):
+            if Ki | tmp_int == Ki:
                 tmp_sum += tmp_val
-            elif (K >> (i + 1)) | ((tmp_int >> (i + 1)) == (K >> (i + 1))):
-                tmp_sum += tmp_val
-        print(str(tmp_sum) + " " + str(i))
+
+        # print(str(tmp_sum) + " " + str(i))
         if max_value < tmp_sum:
             max_value = tmp_sum
 
@@ -29,8 +36,8 @@ if __name__ == '__main__':
     for tmp_int, tmp_val in integer_value:
         if K | tmp_int == K:
             K_val += tmp_val
-    print("max_val" + str(max_value))
-    print("K_val" + str(K_val))
+    # print("max_val" + str(max_value))
+    # print("K_val" + str(K_val))
     if K_val < max_value:
         print(max_value)
     else:
