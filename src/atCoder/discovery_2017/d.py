bf77a46bf5289321ba5e1cac898fc43781429317
nannany <ymym1990ymym@gmail.com>
2017-10-08 19:46:11 +0900

    tochu

--- src/atCoder/discovery_2017/d.py
+++ src/atCoder/discovery_2017/d.py
@@ -1,47 +1,41 @@
-def get_reverse_count(i, j):
-    ret = 0
-    if m[i][j] == "S":
-        ret += 1
-    if m[H - (i + 1)][j] == "S":
-        ret += 1
-    if m[i][W - (j + 1)] == "S":
-        ret += 1
-    if m[H - (i + 1)][W - (j + 1)] == "S":
-        ret += 1
-
-    return ret
-
-
-def judge_nanboku_tozai(i, j):
-    if m[i][j] == "S" and m[H - (i + 1)][j] == "S":
-        return "NS"
-    elif m[i][W - (j + 1)] == "S" and m[H - (i + 1)][W - (j + 1)] == "S":
-        return "NS"
-    elif m[i][j] == "S" and m[i][W - (j + 1)] == "S":
-        return "EW"
-    elif m[H - (i + 1)][j] == "S" and m[H - (i + 1)][W - (j + 1)] == "S":
-        return "EW"
-    else:
-        return "no"
-
-
 if __name__ == '__main__':
-    H, W = list(map(int, input().split()))
-    A, B = list(map(int, input().split()))
+    H, W = map(int, input().split())
+    A, B = map(int, input().split())
     m = [list(input()) for i in range(H)]
 
-    m_for_ans = [[0 for i in range(W // 2)] for j in range(H // 2)]
+    mh = [[0] * W for i in range(H)]
+    mw = [[0] * W for i in range(H)]
+    m4 = [[0] * W for i in range(H)]
 
-    A_con = 0
-    B_con = 0
-    for h in range(H // 2):
-        for w in range(W // 2):
-            m_for_ans[h][w] = get_reverse_count(h, w)
-            if m_for_ans[h][w] == 2:
-                if judge_nanboku_tozai(h, w) == "NS":
-                    A_con += 1
-                elif judge_nanboku_tozai(h, w) == "EW":
-                    B_con += 1
+    # mhに南北対称なものマーク
+    for i in range(H):
+        for j in range(W):
+            if m[i][j] == "S" and m[H - (i + 1)][j] == "S":
+                mh[i][j] = mh[H - (i + 1)][j] = 1
+    # mwに東西対称なものマーク
+    for i in range(H):
+        for j in range(W):
+            if m[i][j] == "S" and m[i][W - (j + 1)] == "S":
+                mw[i][j] = mw[i][W - (j + 1)] = 1
+    # m4に東西南北対称なものマーク
+    for i in range(H):
+        for j in range(W):
+            if mh[i][j] == "S" and mh[i][W - (j + 1)] == "S":
+                m4[i][j] = m4[i][W - (j + 1)] = m4[H - (i + 1)][j] = m4[H - (i + 1)][W - (j + 1)] = 1
 
-    if A_con < B_con :
-        print((A_con +  * A )
+    m4sum = sum(map(sum, m4)) // 4
+    mhsum = sum(map(sum, mh)) // 2 - m4sum
+    mwsum = sum(map(sum, mw)) // 2 - m4sum
+
+    ans = 0
+
+    ans += max(mhsum * A, mwsum * B)
+
+    ans += m4sum * (max(A, B) + A + B)
+
+    ans += A + B
+
+    msum = sum(map(sum, m))
+    if msum == sum(map(sum, mh)) :
+        ans -= A
+    if msum == sum(map(sum, mh)) :
