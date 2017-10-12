712fddaf3528f386ff625ca3cfb0c9aa35355500
nannany <ymym1990ymym@gmail.com>
2017-10-12 23:35:49 +0900

    ac

--- src/atCoder/discovery_2017/d.py
+++ src/atCoder/discovery_2017/d.py
@@ -1,8 +1,12 @@
 if __name__ == '__main__':
     H, W = map(int, input().split())
     A, B = map(int, input().split())
-    m = [list(input()) for i in range(H)]
-
+    m = [[0] * W for i in range(H)]
+    for i in range(H):
+        tmp = input()
+        for j in range(W):
+            if tmp[j] == "S":
+                m[i][j] = 1
     mh = [[0] * W for i in range(H)]
     mw = [[0] * W for i in range(H)]
     m4 = [[0] * W for i in range(H)]
@@ -10,32 +14,32 @@ if __name__ == '__main__':
     # mhに南北対称なものマーク
     for i in range(H):
         for j in range(W):
-            if m[i][j] == "S" and m[H - (i + 1)][j] == "S":
+            if m[i][j] & m[H - (i + 1)][j] == 1:
                 mh[i][j] = mh[H - (i + 1)][j] = 1
     # mwに東西対称なものマーク
     for i in range(H):
         for j in range(W):
-            if m[i][j] == "S" and m[i][W - (j + 1)] == "S":
+            if m[i][j] & m[i][W - (j + 1)] == 1:
                 mw[i][j] = mw[i][W - (j + 1)] = 1
     # m4に東西南北対称なものマーク
     for i in range(H):
         for j in range(W):
-            if mh[i][j] == "S" and mh[i][W - (j + 1)] == "S":
-                m4[i][j] = m4[i][W - (j + 1)] = m4[H - (i + 1)][j] = m4[H - (i + 1)][W - (j + 1)] = 1
+            if mh[i][j] & mh[i][W - (j + 1)] == 1:
+                m4[i][j] = m4[i][W - (j + 1)] = 1
 
-    m4sum = sum(map(sum, m4)) // 4
-    mhsum = sum(map(sum, mh)) // 2 - m4sum
-    mwsum = sum(map(sum, mw)) // 2 - m4sum
+    m4sum = sum(map(sum, m4))
+    mhsum = sum(map(sum, mh)) - m4sum
+    mwsum = sum(map(sum, mw)) - m4sum
 
     ans = 0
 
-    ans += max(mhsum * A, mwsum * B)
-
-    ans += m4sum * (max(A, B) + A + B)
-
+    ans += max(mhsum // 2 * A + m4sum // 4 * (max(A, B) + A + B), mwsum // 2 * B + m4sum // 4 * (max(A, B) + A + B))
     ans += A + B
 
     msum = sum(map(sum, m))
-    if msum == sum(map(sum, mh)) :
+    if msum == sum(map(sum, mh)):
         ans -= A
-    if msum == sum(map(sum, mh)) :
+    if msum == sum(map(sum, mw)):
+        ans -= B
+
+    print(ans)
