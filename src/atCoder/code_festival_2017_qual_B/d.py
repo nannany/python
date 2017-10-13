2d1c9e77a6c196cb65578dd81c9084abb6e00e61
nannany <ymym1990ymym@gmail.com>
2017-10-14 02:05:18 +0900

    とちゅう。ここからのロジックがよくわからない

--- src/atCoder/code_festival_2017_qual_B/d.py
+++ src/atCoder/code_festival_2017_qual_B/d.py
@@ -1,19 +1,13 @@
+import re
+
 if __name__ == '__main__':
     N = int(input())
-    s = input()
+    s = input().strip('0')
+    mojiretu_list = re.split('0{2,}', s)
 
     ans = 0
-    one_accumulate = 0
-    for i in range(N):
-        if s[i:i + 3] == "101":
-            ans += 1 + one_accumulate
-            s = s[:i] + "010" + s[(i + 3):]
-            one_accumulate = 0
-            print(s)
-        else:
-            if s[i] == "1":
-                one_accumulate += 1
-            elif s[i] == "0":
-                one_accumulate = 0
 
-    print(ans)
+    for mojiretu in range(mojiretu_list):
+        ichi_list = re.split('0', mojiretu)
+        if len(ichi_list) == 1:
+            continue
