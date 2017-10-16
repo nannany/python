dd9825e567a3a18a27a1d57e14da24bd7dbfd6ba
nannany <ymym1990ymym@gmail.com>
2017-10-16 23:46:36 +0900

    TLE

--- src/atCoder/beginner_contest_075/d.py
+++ src/atCoder/beginner_contest_075/d.py
@@ -1,58 +1,28 @@
+import itertools
 import copy
-
-
-def get_min_x():
-    min_x = float('inf')
-    for x, y, i in plots:
-        if x < min_x:
-            ret = (x, y, i)
-    return ret
-
-
-def get_max_x():
-    max_x = -float('inf')
-    for x, y, i in plots:
-        if x > max_x:
-            ret = (x, y, i)
-    return ret
-
-
-def get_min_y():
-    min_y = float('inf')
-    for x, y, i in plots:
-        if y < min_y:
-            ret = (x, y, i)
-    return ret
-
-
-def get_max_y():
-    max_y = -float('inf')
-    for x, y, i in plots:
-        if y > max_y:
-            ret = (x, y, i)
-    return ret
-
-
-def get_in_num():
-    ret = 0
-    for x, y, i in org_plots:
-        if min_x[0] <= x <= max_x[0] and min_y[1] <= y <= max_y[1]:
-            ret += 1
-    return ret
 
 if __name__ == '__main__':
     N, K = map(int, input().split())
     plots = []
     for i in range(N):
         tmp_x, tmp_y = map(int, input().split())
-        plots.append((tmp_x, tmp_y, i))
-    org_plots = copy.deepcopy(plots)
-    min_x = get_min_x(plots)
-    max_x = get_max_x(plots)
-    min_y = get_min_y(plots)
-    max_y = get_max_y(plots)
+        plots.append((tmp_x, tmp_y))
+    combi_x = list(itertools.combinations(plots, 2))
+    combi_y = list(itertools.combinations(plots, 2))
+    menseki = float('inf')
+    for x1, x2 in combi_x:
+        for y1, y2 in combi_y:
+            left_x = min(x1[0], x2[0], y1[0], y2[0])
+            right_x = max(x1[0], x2[0], y1[0], y2[0])
+            bottom_y = min(x1[1], x2[1], y1[1], y2[1])
+            top_y = max(x1[1], x2[1], y1[1], y2[1])
+            in_num = 0
+            for x, y in plots:
+                if left_x <= x <= right_x and bottom_y <= y <= top_y:
+                    in_num += 1
 
-    x_leng = max_x - min_x
-    y_leng = max_y - min_y
+            if K <= in_num:
+                if (right_x - left_x) * (top_y - bottom_y) < menseki:
+                    menseki = (right_x - left_x) * (top_y - bottom_y)
 
-    while True:
+    print(menseki)
