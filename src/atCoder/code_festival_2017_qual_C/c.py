4b1ab0909e36b2d4af3ea0b33b77a7a19453ee4d
nannany <ymym1990ymym@gmail.com>
2017-10-23 22:40:04 +0900

    ac

--- src/atCoder/code_festival_2017_qual_C/c.py
+++ src/atCoder/code_festival_2017_qual_C/c.py
@@ -1,42 +1,27 @@
+from collections import deque
 import sys
 
-
-def is_kaibun(mojiretu):
-    for i in range(0, len(mojiretu) // 2):
-        if mojiretu[i] != mojiretu[len(mojiretu) - (i + 1)]:
-            return False
-
-    return True
-
-
 if __name__ == '__main__':
-    s = input()
-    if not is_kaibun(s.replace("x", "")):
-        print(-1)
-        sys.exit()
+    s = list(input())
+    que = deque(s)
 
-    s_rev = s[::-1]
-
-    target = 0
     ans = 0
-    s_list = list(s)
-    s_rev_list = list(s_rev)
-    while True:
-        if len(s) // 2 < target:
-            break
-
-        if s_list[target] == s_rev_list[target]:
-            target += 1
-            continue
-        elif s_list[target] == "x":
-            s_rev_list.insert(target, "x")
-            s_list.insert(len(s_list) - target, "x")
-            ans += 1
-            target += 1
-        elif s_rev_list[target] == "x":
-            s_list.insert(target, "x")
-            s_rev_list.insert(len(s_rev_list) - target, "x")
-            ans += 1
-            target += 1
+    while len(que) != 0:
+        if que[0] == que[-1]:
+            if len(que) == 1:
+                que.pop()
+            else:
+                que.popleft()
+                que.pop()
+        elif que[0] != "x" and que[-1] != "x":
+            print(-1)
+            sys.exit()
+        else:
+            if que[0] == "x":
+                que.append("x")
+                ans += 1
+            elif que[-1] == "x":
+                que.appendleft("x")
+                ans += 1
 
     print(ans)
