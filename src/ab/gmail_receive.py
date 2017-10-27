f4b6d88548eeec9eb891fe7e68187dfb9a843979
nannany <ymym1990ymym@gmail.com>
2017-10-28 00:51:56 +0900

    読解

--- src/ab/gmail_receive.py
+++ src/ab/gmail_receive.py
@@ -20,14 +20,20 @@ APPLICATION_NAME = "MyGmailReceiver"
 
 
 def get_credential():
+    # 自分のパス入れる
     script_dir = os.path.abspath(os.path.dirname(__file__))
+    # credentials のパス入れる
     credential_dir = os.path.join(script_dir, ".credentials")
+    # credentialsなかったらパス作る
     if not os.path.exists(credential_dir):
         os.makedirs(credential_dir)
+    # 情報入ったjsonのパス入れる
     credential_path = os.path.join(credential_dir, "my-gmail-receiver.json")
+    # Storageの概念はよくわからんが、とりあえずcredentials にjsonで認証情報入れてる
     store = Storage(credential_path)
     credentials = store.get()
 
+    # この下よくわからん。FLOWの概念とか
     if not credentials or credentials.invalid:
         flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
         flow.user_agent = APPLICATION_NAME
@@ -41,6 +47,7 @@ def get_credential():
 
 
 def main():
+    # 認証情報入れる
     credentials = get_credential()
     http = credentials.authorize(httplib2.Http())
     service = discovery.build('gmail', 'v1', http=http)
