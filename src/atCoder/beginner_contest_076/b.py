eeb30f550851b6023e3f12e7b78a80ca7944e64d
nannany <ymym1990ymym@gmail.com>
2017-10-31 23:43:27 +0900

    ac

--- src/atCoder/beginner_contest_076/b.py
+++ src/atCoder/beginner_contest_076/b.py
@@ -1,1 +1,12 @@
+if __name__ == '__main__':
+    N = int(input())
+    K = int(input())
 
+    num = 1
+    for i in range(N):
+        if num <= K:
+            num *= 2
+        else:
+            num += K
+
+    print(num)
