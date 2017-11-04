09986d6c7cf1a0e3766ca1e5557f8b6f7b36354a
nannany <ymym1990ymym@gmail.com>
2017-11-04 21:03:26 +0900

    ac

---
+++ src/atCoder/beginner_contest_077/a.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+    C1 = list(input())
+    C2 = list(input())
+    if C1[0] == C2[2] and C1[1] == C2[1]and C1[2] == C2[0]:
+        print("YES")
+    else:
+        print("NO")
