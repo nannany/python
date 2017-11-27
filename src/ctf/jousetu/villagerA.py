d6fdcc5448336a26da223c66e0762b529e299c8e
nannany <ymym1990ymym@gmail.com>
2017-11-28 00:59:08 +0900

    接続はできる

---
+++ src/ctf/jousetu/villagerA.py
@@ -0,0 +1,17 @@
+import paramiko
+
+if __name__ == '__main__':
+    host = 'ctfq.sweetduet.info'
+    username = 'q4'
+    password = 'q60SIMpLlej9eq49'
+
+    client = paramiko.SSHClient()
+    client.load_system_host_keys()
+    client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+    client.connect(host, username=username, password=password, look_for_keys=False, allow_agent=False, port=10022)
+
+    stdin, stdout, stderr = client.exec_command('ls -l')
+
+    for o in stdout:
+        print(o)
+
