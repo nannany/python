859e89288a577a3fe74db5f11988e8096d6f3cdc
nannany <ymym1990ymym@gmail.com>
2018-01-09 23:39:26 +0900

    ac.ほぼ答えみた

--- src/atCoder/regular_contest_087/d.py
+++ src/atCoder/regular_contest_087/d.py
@@ -1,37 +1,29 @@
+def c(p, g, lis):
+    for e in lis:
+        if p <= g:
+            p += e
+        else:
+            p -= e
+    return p == g
+
+
 if __name__ == '__main__':
     s = input()
-    x, y = map(int, input().split())
+    gx, gy = map(int, input().split())
 
     s = s.split("T")
     s = [len(_) for _ in s]
 
-    x_move = [ele for (i, ele) in enumerate(s) if i % 2 == 0]
-    y_move = [ele for (i, ele) in enumerate(s) if i % 2 == 1]
+    x_move = s[::2]
+    y_move = s[1::2]
 
-    dpx = [[False for _ in range(sum(x_move) * 2 + 1)] for _ in range(len(x_move) + 1)]
+    x = x_move.pop(0)
+    y = 0
 
-    dpx[1][x_move[0]] = True
-    for i, _x in enumerate(x_move):
-        if i == 0:
-            continue
+    x_move.sort(reverse=True)
+    y_move.sort(reverse=True)
 
-        for j in range(-sum(x_move), sum(x_move) + 1):
-            dpx[i + 1][j] = dpx[i][j - _x] or dpx[i][j + _x]
-
-    dpy = [[False for _ in range(sum(y_move) * 2 + 1)] for _ in range(len(y_move) + 1)]
-
-    dpy[0][0] = True
-    for i, _y in enumerate(y_move):
-        for j in range(-sum(y_move), sum(y_move) + 1):
-            dpy[i + 1][j] = dpy[i][j - _y] or dpy[i][j + _y]
-
-    try:
-        dpx[len(x_move)][x] and dpy[len(y_move)][y]
-    except:
-        print("No")
-        exit()
-
-    if dpx[len(x_move)][x] and dpy[len(y_move)][y]:
+    if c(x, gx, x_move) and c(y, gy, y_move):
         print("Yes")
     else:
         print("No")
