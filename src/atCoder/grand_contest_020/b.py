ac5bb36b42032d52f5022626a08285030ee9c06a
nannany <ymym1990ymym@gmail.com>
2018-01-14 22:58:34 +0900

    torima

--- src/atCoder/grand_contest_020/b.py
+++ src/atCoder/grand_contest_020/b.py
@@ -2,38 +2,16 @@ if __name__ == '__main__':
     K = int(input())
     A = list(map(int, input().split()))
 
-    A_rev = reversed(A)
-
     # min
-    min_target_nums = [2]
-    for i, a in enumerate(A_rev):
-        if i == 0 and a != 2:
-            print(-1)
-            exit()
-
-        targets = [_ for _ in min_target_nums if _ % a == 0]
+    target_nums = [2]
+    for a in reversed(A):
+        targets = [_ for _ in target_nums if _ % a == 0]
         if len(targets) == 0:
             print(-1)
             exit()
-        target = min(targets)
 
-        min_target_nums = [_ for _ in range(a)]
-        min_target_nums = list(map(lambda x: x + target, min_target_nums))
+        target_nums = []
+        for target in targets:
+            target_nums += [_ + target for _ in range(a)]
 
-    # max
-    max_target_nums = [2]
-    for i, a in enumerate(A_rev):
-        if i == 0 and a != 2:
-            print(-1)
-            exit()
-
-        targets = [_ for _ in max_target_nums if _ % a == 0]
-        if len(targets) == 0:
-            print(-1)
-            exit()
-        target = max(targets)
-
-        max_target_nums = [_ for _ in range(a)]
-        max_target_nums = list(map(lambda x: x + target, max_target_nums))
-
-    print(str(min(min_target_nums)) + " " + str(max(max_target_nums)))
+    print(str(min(target_nums)) + " " + str(max(target_nums)))
