922844068bdc9d2ec2f86fc6a4bde32ace24ba09
nannany <ymym1990ymym@gmail.com>
2018-01-28 23:26:15 +0900

    torima

---
+++ src/atCoder/regular_contest_090/c.py
@@ -0,0 +1,15 @@
+def main():
+    N = int(input())
+    A1 = list(map(int, input().split()))
+    A2 = list(map(int, input().split()))
+    ans = 0
+    for i in range(N):
+        tmp = sum(A1[:(i + 1)]) + sum(A2[i:])
+        if tmp > ans:
+            ans = tmp
+
+    print(ans)
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/atCoder/regular_contest_090/d.py
@@ -0,0 +1,3 @@
+if __name__ == '__main__':
+    N, M = map(int, input().split())
+
