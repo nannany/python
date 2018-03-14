59abd03c8252b2b5d3275a80ff830dc79cc2d48e
nannany <ymym1990ymym@gmail.com>
2018-03-15 00:24:55 +0900

    まだTLE。謎

--- src/atCoder/easy/ATC001_深さ優先探索.py
+++ src/atCoder/easy/ATC001_深さ優先探索.py
@@ -1,38 +1,39 @@
-import queue
+from collections import deque
 
 dif = [(-1, 0), (1, 0), (0, -1), (0, 1)]
 
 
 def dfs(_y, _x):
     global c
-    q = queue.Queue()
-    q.put([_y, _x])
-    while not q.empty():
-        tmp_y, tmp_x = q.get()
+    q = deque()
+    q.append([_y, _x])
+    while q:
+        tmp_y, tmp_x = q.pop()
         c[tmp_y][tmp_x] = "#"
         for d in dif:
             if 0 <= tmp_x + d[1] <= W - 1 and 0 <= tmp_y + d[0] <= H - 1:
                 if c[tmp_y + d[0]][tmp_x + d[1]] == "g":
-                    return True
+                    print("Yes")
+                    exit()
                 elif c[tmp_y + d[0]][tmp_x + d[1]] == ".":
-                    q.put([tmp_y + d[0], tmp_x + d[1]])
-    return False
+                    q.append([tmp_y + d[0], tmp_x + d[1]])
+                    continue
+                else:
+                    continue
+            else:
+                continue
 
+H, W = map(int, input().split())
+c = [[] for _ in range(H)]
+for i in range(H):
+    c[i] = list(input())
+    try:
+        tmp = c[i].index('s')
+    except:
+        tmp = -1
+    if tmp > -1:
+        x = tmp
+        y = i
 
-if __name__ == '__main__':
-    H, W = map(int, input().split())
-    c = [[] for _ in range(H)]
-    for i in range(H):
-        c[i] = list(input())
-        try:
-            tmp = c[i].index('s')
-        except:
-            tmp = -1
-        if tmp > -1:
-            x = tmp
-            y = i
-
-    if dfs(y, x):
-        print("Yes")
-    else:
-        print("No")
+dfs(y, x)
+print("No")
