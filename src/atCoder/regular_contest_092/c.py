904138499606f9f7f544dd2c8c3286b3fce64b20
nannany <ymym1990ymym@gmail.com>
2018-03-17 21:42:52 +0900

    wa

--- src/atCoder/regular_contest_092/c.py
+++ src/atCoder/regular_contest_092/c.py
@@ -1,16 +1,8 @@
+from operator import itemgetter
 
 
-if __name__ == '__main__':
-    N = int(input())
-    red = [list(map(int, input().split())) for _ in range(N)]
-    blue = [list(map(int, input().split())) for _ in range(N)]
-
-    red.sort(reverse=True)
-    blue.sort()
-
-    # print(red)
-    # print(blue)
-    ans_1 = 0
+def ret(red, blue):
+    ans = 0
     for b_x, b_y in blue:
         del_flg = False
         for r_x, r_y in red:
@@ -22,11 +14,27 @@ if __name__ == '__main__':
             del_flg = True
             del_x = r_x
             del_y = r_y
-            ans_1 += 1
+            ans += 1
             break
         if del_flg:
             red.remove([del_x, del_y])
 
-    ans_
+    return ans
 
-    print(ans_1)
+
+if __name__ == '__main__':
+    N = int(input())
+    red = [list(map(int, input().split())) for _ in range(N)]
+    blue = [list(map(int, input().split())) for _ in range(N)]
+
+    red1 = sorted(red, reverse=True, key=itemgetter(0))
+    blue1 = sorted(blue, key=itemgetter(0))
+
+    ans_1 = ret(red1, blue1)
+
+    red2 = sorted(red, reverse=True, key=itemgetter(1))
+    blue2 = sorted(blue, key=itemgetter(1))
+
+    ans_2 = ret(red2, blue2)
+
+    print(max(ans_1, ans_2))
