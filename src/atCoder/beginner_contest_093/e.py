0fdb444a437e66a62cdc463f95867bfb5ca1acc0
nannany <ymym1990ymym@gmail.com>
2018-04-17 00:33:25 +0900

    さすがにだめ

--- src/atCoder/beginner_contest_093/e.py
+++ src/atCoder/beginner_contest_093/e.py
@@ -2,9 +2,12 @@ if __name__ == '__main__':
     N = int(input())
     A = []
     B = []
+    ans = 0
     for _ in range(N):
         a, b = map(int, input().split())
+        if a < b:
+            ans += b
         A.append(a)
         B.append(b)
-    print(A)
-    print(B)
+
+    print(ans)
